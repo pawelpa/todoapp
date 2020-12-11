ba501dc40a101f7c8ff45838f09e76dd29f76ef8
Paweł Pasternak <ppaste@gmail.com>
2020-12-11 11:40:47 +0100

    deploy

---
+++ precache-manifest.05856c7bc7f0cd673ef042b7f051e032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea863266f92403bc1e3a",
+    "url": "/todoapp/css/app.8d70d042.css"
+  },
+  {
+    "revision": "048c7db913889b62bb5a9cf4062aed6b",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "ea863266f92403bc1e3a",
+    "url": "/todoapp/js/app.a726b5b5.js"
+  },
+  {
+    "revision": "2fc4530dfda95f7cda6a",
+    "url": "/todoapp/js/chunk-vendors.a957d548.js"
+  },
+  {
+    "revision": "f02e9b2647527bee9728b951a76a012d",
+    "url": "/todoapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todoapp/robots.txt"
+  }
+]);
